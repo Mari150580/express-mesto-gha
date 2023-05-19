70876a89d1da3b9bc1b64c530b521f19e7230e4d
Марат Рохов <chulkova80@inbox.ru>
2023-05-19 13:38:54 +0300

    sprint14

--- config.js
+++ config.js
@@ -3,6 +3,14 @@ const ERROR_NOT_FOUND = 404;
 const USER_EXISTS = 409;
 const INCORRECT_DATA = 401;
 
-const URL_REGEXP = /http[s]?:\/\/(www\.)?[\w\d\-\._~:\/?#\[\]@!\$&'()*+,;=]+#?/im;
+const URL_REGEXP = /http[s]?:\/\/(www\.)?[\w\d\-._~:/?#[\]@!$&'()*+,;=]+#?/im;
 
-module.exports = { ERROR_BAD_REQUEST, ERROR_NOT_FOUND, USER_EXISTS, INCORRECT_DATA, URL_REGEXP };
+module.exports = {
+  ERROR_BAD_REQUEST,
+  ERROR_NOT_FOUND,
+  USER_EXISTS,
+  INCORRECT_DATA,
+  URL_REGEXP,
+};
+ // const URL_REGEXP = /http[s]?:\/\/(www\.)?[\w\d\-\._~:\/?#\[\]@!\$&'()*+,;=]+#?/im;
+
